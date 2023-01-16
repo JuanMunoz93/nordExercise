74ff61b39eb01c26c9a0631483359e962699b9a2
Juan Munoz <juano.mube@gmail.com>
2023-01-16 21:44:06 +0100

    locators updated

--- cypress/integration/pages/checkoutPage.js
+++ cypress/integration/pages/checkoutPage.js
@@ -4,13 +4,19 @@ export class CheckoutPage{
 
     webElements = {
         loginLint: () => cy.get("a[data-testid='UserProfile-login-button']"),
-        oneYearSubBtn: () => cy.xpath('//button[@data-testid="MainPlanCard-button" and contains(.,"45") and not(@disabled)]', { timeout: 10000 }),
-        oneYearSubPriceLbl: () => cy.xpath('//button[@data-testid="MainPlanCard-button" and contains(.,"45")]//span[@data-testid="TotalBilledText-total-price"]'),
         proceedToPaymentBtn: () => cy.xpath('//a[@data-testid="PaymentButton" and not(@disabled)]'),
+        
+        //oneYearSubBtn: () => cy.xpath('//button[@data-testid="MainPlanCard-button" and contains(.,"45") and not(@disabled)]', { timeout: 10000 }),
+        //oneYearSubPriceLbl: () => cy.xpath('//button[@data-testid="MainPlanCard-button" and contains(.,"45")]//span[@data-testid="TotalBilledText-total-price"]'),
+        allSubsOptions: () => cy.get('button[data-testid="MainPlanCard-button"]'),
+        allSubsPriceLbl: () => cy.get('button[data-testid="MainPlanCard-button"] span[data-testid="TotalBilledText-total-price"]'),
+        disabledBtns: () => cy.get('button[data-testid="MainPlanCard-button"]:disabled'),
     }
 
     waitUntilSiteLoads(){
-        this.webElements.oneYearSubBtn().should('be.visible');
+        //this.webElements.oneYearSubBtn().should('be.visible');
+
+        this.webElements.disabledBtns().should('have.length', 0);
     }
 
     goToLogin(){
@@ -18,11 +24,23 @@ export class CheckoutPage{
     }
 
     selectOneYearPlan(){
-        this.webElements.oneYearSubBtn().click();
+        //this.webElements.oneYearSubBtn().click();
+        //const plans = this.webElements.allSubsOptions();
+        
+        this.webElements.allSubsOptions().eq(1).click();
     }
 
     getOneYearSubscriptionPrice(){
+
+        /*
         this.webElements.oneYearSubPriceLbl()
+        .invoke('text')
+        .then((elementText) => {
+            elementText=elementText.replace('€', '');
+            cy.wrap(elementText).as('oneYearSubPriceOnSelection')
+        })*/
+        
+        this.webElements.allSubsPriceLbl().eq(1)
         .invoke('text')
         .then((elementText) => {
             elementText=elementText.replace('€', '');
